7c0a922143e0b6341ef071cd32278a6e4a0b8277
Endoze <endoze@endozemedia.com>
2012-06-25 18:10:38 -0400

    Added TrackedObject to model and created an Entity for it.

---
+++ LARA/TrackedObject.h
@@ -0,0 +1,22 @@
+//
+//  TrackedObject.h
+//  LARA
+//
+//  Created by Chris Stephan on 6/25/12.
+//  Copyright (c) 2012 Endozemedia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface TrackedObject : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * subtitle;
+@property (nonatomic, retain) NSString * iconImageType;
+@property (nonatomic, retain) NSString * iconImageColor;
+@property (nonatomic, retain) NSNumber * lat;
+@property (nonatomic, retain) NSNumber * lon;
+
+@end
---
+++ LARA/TrackedObject.m
@@ -0,0 +1,21 @@
+//
+//  TrackedObject.m
+//  LARA
+//
+//  Created by Chris Stephan on 6/25/12.
+//  Copyright (c) 2012 Endozemedia. All rights reserved.
+//
+
+#import "TrackedObject.h"
+
+
+@implementation TrackedObject
+
+@dynamic name;
+@dynamic subtitle;
+@dynamic iconImageType;
+@dynamic iconImageColor;
+@dynamic lat;
+@dynamic lon;
+
+@end
