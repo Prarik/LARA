a9496ccf5e84e0758fea115033c61df09754077c
Endoze <endoze@endozemedia.com>
2012-06-25 16:47:53 -0400

    Initial Commit

---
+++ LARA/LARAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LARAppDelegate.h
+//  LARA
+//
+//  Created by Chris Stephan on 6/25/12.
+//  Copyright (c) 2012 Endozemedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ LARA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LARA
+//
+//  Created by Chris Stephan on 6/25/12.
+//  Copyright (c) 2012 Endozemedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LARAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LARAppDelegate class]));
+    }
+}
